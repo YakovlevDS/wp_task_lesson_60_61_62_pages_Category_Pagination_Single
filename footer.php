122e4dc34f9d8b1b7dc7c3a9d2f3589d6afbc715
Dmitriy Yakovlev <1d3ddd@gmail.com>
2021-12-02 20:02:21 +0200

    added pages Category and Single dynamic

---
+++ footer.php
@@ -0,0 +1,44 @@
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="row">
+                    <?php dynamic_sidebar('sidebar-footer1') ?>
+                </div>
+            </div>
+            <!-- /.col-md-6 -->
+
+            <div class="col-md-6 footer-images">
+                <div class="row">
+                    <div class="col-md-12">
+                        <?php dynamic_sidebar('sidebar-footer2') ?>
+                    </div>
+                </div>
+            </div>
+            <!-- /.col-md-6 -->
+        </div>
+    </div>
+</footer>
+<!-- /.footer -->
+
+<div class="copyright">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <ul class="nav">
+                    <li class="nav-item">
+                        <span class="nav-link">&copy; 2022 Theme by psdfreebies.com</span>
+                    </li>
+                    <li class="nav-item"><a href="#" class="nav-link">Privacy</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Terms of Use</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Site Map</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- /.copyright -->
+
+<?php wp_footer() ?>
+</body>
+</html>
